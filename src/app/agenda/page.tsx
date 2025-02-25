660079f831cf17f0fbd2944e345d3be35bcd69f3
LucasCrespo75 <lucascrespo_rodrigues@live.com>
2025-02-25 16:39:10 -0300

    feat add: Agenda ajustada de acordo com o prototipo(necessario fontes e coisas afins)

--- src/app/agenda/page.tsx
+++ src/app/agenda/page.tsx
@@ -19,10 +19,10 @@ export default function AgendaPage() {
   };
 
   return (
-    <section className="relative pt-[100px] bg-white max-h-screen ">
+    <section className="relative pt-[100px] bg-white mixn-h-screen ">
       <div className="container mx-auto px-4 py-8 flex gap-20">
         {/* Sidebar com Calendário e Filtros */}
-        <aside className="w-1/2 flex flex-col gap-4">
+        <aside className="w-1/4 flex flex-col gap-4">
           <div className="p-4"> {/* Removendo classes de layout flexível */}
             <div className="card text-black " style={{ width: '350px' }}> {/* Adicionando tamanho fixo */}
               <Calendar
@@ -34,22 +34,13 @@ export default function AgendaPage() {
             </div>
           </div>
           <Filters />
-          <button className="w-full mt-4 bg-green-500 text-[#00000] py-2 rounded-lg">
-            Ver todas as reuniões
-          </button>
+
         </aside>
 
         {/* Conteúdo das Sessões */}
         <section className="w-3/4">
-          {selectedDate && (
-            <p className="mt-4 text-lg text-black ">
-              Data selecionada:{" "}
-              <span className="font-bold text-black">
-                {selectedDate.toLocaleDateString("pt-BR")}
-              </span>
-            </p>
-          )}
-          <h2 className="text-xl font-bold mb-4">
+         
+          <h2 className="text-xl text-black font-medium mb-4">
             {selectedDate
               ? selectedDate.toLocaleDateString("pt-BR", {
                 weekday: "long",
@@ -58,6 +49,7 @@ export default function AgendaPage() {
               })
               : "Selecione uma data"}
           </h2>
+          <h2 className="text-black text-5xl font-bold">SESSÕES</h2>
 
           <div className="space-y-4">
             <SessionCard
